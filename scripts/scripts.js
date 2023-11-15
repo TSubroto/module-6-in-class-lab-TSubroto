358ddc35e5f6d234a540593c844805912852959d
tsubroto <timothy_subroto@yahoo.com>
2023-11-14 19:37:05 -0500

    phase 1 partially complete

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+/*
+    JavaScript Basics Tutorial
+*/
+
+let textValue = "Hello World";
+const headingElement = document.querySelector('h1');
+headingElement.textconstant = testValue;
+
+const imageElement = document.querySelector('img');
+//imageElement.style.display = "none";
+
+imageElement.classList.add('gone');
