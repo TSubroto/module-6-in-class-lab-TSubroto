2fd927adc2116f60451c0504a29af53f82dbde2e
tsubroto <timothy_subroto@yahoo.com>
2023-11-21 19:16:56 -0500

    Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,1 @@
-/*
-    JavaScript Basics Tutorial
-*/
-
-let testValue = "Hello World";
-const headingElement = document.querySelector('h1');
-headingElement.textconstant = testValue;
-
-const imageElement = document.querySelector('img');
-//imageElement.style.display = "none";
-
-imageElement.classList.add('gone');
+/*JavaScript Basics Tutorial*/
